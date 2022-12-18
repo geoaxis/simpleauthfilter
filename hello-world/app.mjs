9869e7de33606ee0f3d193ac0135dfd55f563947
Hatim aka geoaxis <72706+geoaxis@users.noreply.github.com>
2022-12-18 19:12:04 +0100

    object make the token object return directly

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -12,10 +12,52 @@
  */
 
 import * as http from 'http';
+import * as https from 'https';
+import * as qs from 'querystring';
 
-export const lambdaHandler = (event, context) => {
+
+async function post(url, data, authToken) {
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'authorization': 'Basic ' + authToken,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        timeout: 1000, // in ms
+    }
+
+    return new Promise((resolve, reject) => {
+        const req = https.request(url, options, (res) => {
+            if (res.statusCode < 200 || res.statusCode > 299) {
+                return reject(new Error(`HTTP status code ${res.statusCode}`))
+            }
+
+            const body = []
+            res.on('data', (chunk) => body.push(chunk))
+            res.on('end', () => {
+                const resString = Buffer.concat(body).toString()
+                resolve(resString)
+            })
+        })
+
+        req.on('error', (err) => {
+            reject(err)
+        })
+
+        req.on('timeout', () => {
+            req.destroy()
+            reject(new Error('Request time out'))
+        })
+
+        req.write(data)
+        req.end()
+    })
+}
+
+
+export const lambdaHandler = async (event, context) => {
     try {
-        let secret = '';
         let headerToken = process.env.AWS_SESSION_TOKEN;
         let ssmPort = process.env.PARAMETERS_SECRETS_EXTENSION_HTTP_PORT;
 
@@ -26,27 +68,50 @@ export const lambdaHandler = (event, context) => {
         const options = {
             hostname: 'localhost',
             port: ssmPort,
-            path: '/systemsmanager/parameters/get?name=%2Flambda%2Fbasicauth%2Fauthtest',
+            path: '/systemsmanager/parameters/get?name=%2Flambda%2Fbasicauth%2Fauthtest&withDecryption=true',
             headers: {
                 'X-Aws-Parameters-Secrets-Token': headerToken
+
             }
         }
 
-        http.get(options, (resp) => {
-            resp.on('data', (chunk) => {
-                secret += chunk;
-            });
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
+        const res = await new Promise(resolve => {
+            http.get(options, resolve);
         });
 
+        // A ServerResponse is a readable stream, so you need to use the
+        // stream-to-promise pattern to use it with async/await.
+        let data = await new Promise((resolve, reject) => {
+            let data = '';
+            res.on('data', chunk => data += chunk);
+            res.on('error', err => reject(err));
+            res.on('end', () => resolve(data));
+        });
 
-        console.log("Secret is :" + secret);
+        data = JSON.parse(data);
+        let authToken = data.Parameter.Value;
 
-        console.log(event);
-        console.log(context);
 
-        return event;
+        var postData = qs.stringify({
+            'grant_type': 'client_credentials',
+            'scope': 'transactions/post'
+        });
+
+        const tokenResponse = await post('https://anonymoustest1.auth.eu-west-1.amazoncognito.com/oauth2/token', postData, authToken);
+
+        const response = {
+            "statusCode": 200,
+            "headers": {
+                "Cache-Control": "no-cache, no-store, must-revalidate",
+                "Pragma": "no-cache",
+                "Expires": 0
+            },
+            "body": tokenResponse,
+            "isBase64Encoded": false
+        };
+
+        return response;
+
     }
     catch (err) {
         console.log(err);
